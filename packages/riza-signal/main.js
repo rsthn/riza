b41b487ab9e0a41ec06f75fcd2b34484bac13175
Jonathan Palencia <strikeone88@rsthn.com>
2023-10-10 23:26:38 -0600

    [riza-signal] Added support for typed signals.

--- packages/riza-signal/main.js
+++ packages/riza-signal/main.js
@@ -1,10 +1,5 @@
 
 /**
- * Indicates if signal debugging is enabled. When so, every signal change will be logged to the console.
- */
-export let debugSignals = false;
-
-/**
  * Signal class.
  */
 export class Signal
@@ -12,61 +7,146 @@ export class Signal
     #value;
     #defvalue;
     #listeners;
-    #label;
+    #type;
 
-    constructor (value, defvalue)
+    /**
+     * Creates a new signal object.
+     * @param {*} value
+     * @param {*} defvalue
+     */
+    constructor (value=null, defvalue=null)
     {
         this.#value = value;
-        this.#label = null;
+        this.#type = null;
+
+        if (value !== null) {
+            if (typeof value === 'string')
+                this.#type = 'string';
+            else if (typeof value === 'number')
+                this.#type = ~~value === value ? 'int' : 'number';
+            else if (typeof value === 'boolean')
+                this.#type = 'bool';
+        }
 
         this.#defvalue = defvalue;
         this.#listeners = [];
     }
 
+    /**
+     * Sets the type of the signal.
+     * @param {'string'|'bool'|'int'|'number'|'*'} type 
+     * @returns {Signal}
+     */
+    as(type) {
+        this.#type = type === '*' ? null : type;
+        return this;
+    }
+
+    /**
+     * Transforms the given value to the type of the signal.
+     * @param {*} val 
+     * @returns {*}
+     */
+    transform (val)
+    {
+        if (this.#type === null)
+            return val;
+
+        if (this.#type === 'string')
+            return typeof val === 'string' ? val : val.toString();
+
+        if (this.#type === 'int')
+            return ~~val;
+
+        if (this.#type === 'number')
+            return typeof val === 'number' ? val : parseFloat(val);
+
+        if (this.#type === 'bool')
+        {
+            if (typeof val === 'boolean')
+                return val;
+
+            if (typeof val === 'string')
+                return val === 'true' || val === '1';
+
+            return !!val;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the type of the signal.
+     * @returns {'string'|'bool'|'int'|'number'|'*'}
+     */
+    get type() {
+        return this.#type === null ? '*' : this.#type;
+    }
+
+    /**
+     * Returns the value of the signal.
+     * @returns {*}
+     */
     get value() {
         return this.#value;
     }
 
+    /**
+     * Sets the value of the signal.
+     * @param {*} val
+     */
     set value(val) {
+        val = this.transform(val);
         if (this.#value === val) return;
         this.#value = val;
         this.notify();
     }
 
+    /**
+     * Returns the value of the signal.
+     * @returns {*}
+     */
     get() {
         return this.value;
     }
 
+    /**
+     * Sets the value of the signal, when `forced` is true the signal will be updated even if its value is the same.
+     * @param {*} value
+     * @param {boolean} [forced]
+     * @returns {Signal}
+     */
     set(value, forced=false) {
+        value = this.transform(value);
         if (this.#value === value && forced !== true) return;
         this.#value = value;
         this.notify();
         return this;
     }
 
-    label (value=null)
-    {
-        if (value === null)
-            return this.#label;
-        this.#label = value;
-        return this;
-    }
-
+    /**
+     * Resets the value of the signal to its default value.
+     * @returns {Signal}
+     */
     reset() {
         this.value = this.#defvalue;
         return this;
     }
 
+    /**
+     * Connects a callback to the signal.
+     * @param {function} callback
+     * @returns {Signal}
+     */
     connect (callback) {
         this.#listeners.push(callback);
         return this;
     }
 
-    notify()
-    {
-        if (debugSignals)
-            console.log('CHANGED', this.#label, this.#listeners.length);
-
+    /**
+     * Notifies all the listeners of the signal that it has changed its value.
+     */
+    notify() {
         for (let callback of this.#listeners)
             callback();
     }
